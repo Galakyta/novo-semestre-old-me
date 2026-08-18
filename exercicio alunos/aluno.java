01a18c99357c1050d4cfb452116f16d16eb3b1b1
MARCOS VINICIUS DA ROCHA SCHIAVON FERREIRA <marcos.vinicius06@cs.up.edu.br>
2026-08-18 20:14:04 -0300

    AAAAAAA

---
+++ exercicio alunos/aluno.java
@@ -0,0 +1,15 @@
+
+import java.util.List;
+
+public class aluno {
+    protected String nome;
+    protected String registro;
+    public List<String> Disciplinas; 
+    
+
+    public aluno(String nome, String registro) {
+        this.nome = nome;
+        this.registro = registro;
+    }
+
+}
